b66caab9d9ac33b8ea7c68253ccedcae3cd71aa5
rodriguezgordillosantiagogg-ship-it <rodriguezgordillosantiagogg@gmail.com>
2026-01-19 21:39:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-const pageFlip = new St.PageFlip(
-  document.getElementById("book"),
-  {
-    width: 400,
-    height: 500,
-    size: "fixed",
-    showCover: true,
-    mobileScrollSupport: false
-  }
-);
+document.addEventListener("DOMContentLoaded", function () {
 
-pageFlip.loadFromHTML(document.querySelectorAll(".page"));
+  const pageFlip = new St.PageFlip(
+    document.getElementById("book"),
+    {
+      width: 400,
+      height: 500,
+      size: "fixed",
+      showCover: true,
+      mobileScrollSupport: false
+    }
+  );
+
+  pageFlip.loadFromHTML(document.querySelectorAll(".page"));
+
+  document.getElementById("next").onclick = () => pageFlip.flipNext();
+  document.getElementById("prev").onclick = () => pageFlip.flipPrev();
+
+});
